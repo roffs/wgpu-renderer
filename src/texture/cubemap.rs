2708cd94e3eb478dafe8fa569efb0143ffd0b165
Gerard <gerardfabrego@gmail.com>
2024-06-24 14:50:18 +0200

    Fix cubemap texture format

--- src/texture/cubemap.rs
+++ src/texture/cubemap.rs
@@ -67,7 +67,7 @@ impl CubeMap {
             mip_level_count: 1,
             sample_count: 1,
             dimension: wgpu::TextureDimension::D2,
-            format: wgpu::TextureFormat::Bgra8UnormSrgb,
+            format: wgpu::TextureFormat::Rgba8UnormSrgb,
             usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::RENDER_ATTACHMENT,
             view_formats: &[],
         });
